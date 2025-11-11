5bd4bee3ba4e5a8b2bde558eae0452cfb54a2e7a
Hasforth <benjaminhasforth@gmail.com>
2025-11-11 22:45:26 +0100

    Initial

---
+++ src/main.c
@@ -0,0 +1,102 @@
+#include "raylib.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+
+#define WIDTH 400
+#define HEIGHT 400
+#define TITLE "Balls and their admirers"
+#define BALL_COUNT 100
+#define FPS 60
+#define VEL_MAX 5
+#define RADIUS_MAX 20
+#define RADIUS_MIN 5
+
+Color COLORS[] = {
+    LIGHTGRAY, GRAY,   DARKGRAY, YELLOW,     GOLD,      ORANGE,  PINK,
+    RED,       MAROON, GREEN,    LIME,       DARKGREEN, SKYBLUE, BLUE,
+    DARKBLUE,  PURPLE, VIOLET,   DARKPURPLE, BEIGE,     BROWN,   DARKBROWN,
+};
+
+// Definition of Ball
+// Ball stores state and other properties
+typedef struct Ball {
+  float posx;
+  float posy;
+  float velox;
+  float veloy;
+  float size;
+  float color;
+} Ball;
+
+Ball balls[BALL_COUNT];
+
+// Initializes a ball with random values
+Ball *init_ball_random(Ball *p) {
+  // Randomly initialize state and properties
+  p-> posx = rand() % WIDTH;
+  p-> posy = rand() % HEIGHT;
+  p-> velox = rand() % VEL_MAX;
+  p-> veloy = rand() % VEL_MAX;
+  p-> size = rand() RADIUS_MAX;
+  p-> color = rand() % COLORS;
+
+  // Find a leading ball other than the initialized ball itself.
+  Ball *leader; // Represents the leading ball that this ball will follow
+  leader = balls(rand(BALL_COUNT));
+
+  return p;
+}
+
+// Initialize all `balls`
+void init_balls_random() {
+  for (size_t count = 0; count < BALL_COUNT; balls++){
+    Ball iniballs;
+    init_balls_random(iniballs);
+    balls[count];
+  }
+}
+
+Ball *draw_ball(Ball *p) {
+  DrawCircle(p->posx, p->posy, p->radius, p->color);
+  return p;
+}
+
+// Updates the positions of balls according to their velocities
+Ball *update_pos(Ball *p) {
+  p->posx = (WIDTH + p->posx + p->velx) %
+            WIDTH; // `WIDTH +` because C uses truncated division
+  p->posy = (HEIGHT + p->posy + p->vely) % HEIGHT;
+  return p;
+}
+
+// Updates the velocity of a ball so that it follows the leading ball
+Ball *update_vel_for_following(Ball *p) {
+  int errx = p->follows->posx - p->posx;
+  int erry = p->follows->posy - p->posy;
+  p->velx = errx > 0 ? 1 : -1;
+  p->vely = erry > 0 ? 1 : -1;
+  return p;
+}
+
+// Update all elements
+void update_elements() {
+  for (size_t i = 0; i < _Countof balls; ++i) {
+    draw_ball(update_pos(update_vel_for_following(&balls[i])));
+  }
+}
+
+int main() {
+  InitWindow(WIDTH, HEIGHT, TITLE);
+  SetTargetFPS(FPS);
+
+  init_balls_random();
+
+  while (!WindowShouldClose()) // Detect window close button or ESC key
+  {
+    BeginDrawing();
+    update_elements();
+    ClearBackground(RAYWHITE);
+    EndDrawing();
+  }
+}
